8433a617f32828a86af40edd3295ca7ea2e2cb96
Jonk09 <jonathasferreira921@gmail.com>
2025-06-15 12:15:22 -0300

    problemas4

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,24 @@
 <template>
- <bemvindo />
+ <home />
   
 </template>
 
 <script setup>
-  // import alertas from '@/components/alertas.vue'
-   import bemvindo from '@/components/bemvindo.vue'
-  // import cadastro from '@/components/cadastro.vue'
-  // import conexao from '@/components/conexao.vue'
-  // import contato from '@/components/contato.vue'
-  // import decisoes from '@/components/decisoes.vue'
-  // import escalabilidade from '@/components/escalabilidade.vue'
-  // import estoque from '@/components/estoque.vue'
-  // import home from '@/components/home.vue'
-  // import integracao from '@/components/integracao.vue'
-  // import login from '@/components/login.vue'
-  // import reducao from '@/components/reducao.vue'
-  // import sobre from '@/components/sobre.vue'
-  // import suaconta from '@/components/suaconta.vue'
+  //  import alertas from '@/components/alertas.vue'
+  //  import bemvindo from '@/components/bemvindo.vue'
+  //  import cadastro from '@/components/cadastro.vue'
+  //  import conexao from '@/components/conexao.vue'
+  //  import contato from '@/components/contato.vue'
+  //  import decisoes from '@/components/decisoes.vue'
+  //  import escalabilidade from '@/components/escalabilidade.vue'
+  //  import estoque from '@/components/estoque.vue'
+   import home from '@/components/home.vue'
+  //  import integracao from '@/components/integracao.vue'
+  //  import login from '@/components/login.vue'
+  //  import reducao from '@/components/reducao.vue'
+  //  import sobre from '@/components/sobre.vue'
+  //  import suaconta from '@/components/suaconta.vue'
+
 
 
 
